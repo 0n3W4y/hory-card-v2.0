a6f9171c5f8497b7d2c8ce84afde641f64115b5f
0n3W4y <vulturetm@gmail.com>
2015-11-10 10:19:55 +0300

    Update battle.js

--- battle.js
+++ battle.js
@@ -17,33 +17,46 @@ var card_costs = {
   14: 11,
 }; // не уверен что это нужно, но спер у тебя. Посмотрим, может пристрою или видоизменю.
 
-function getRandomFromArr(arr) {                  // достаем произвольное индекс из массива
-  return Math.floor(Math.random() * arr.length);
-}; 
 
-function createCard(){                          // создаем карту, на выходе имеем текстовое название карты типа 12@spades.
+var cardsDeck = 52; //количество кард в колоде
+var playerCardDeck = []; //Колода карт игрока, для взятие карт в руки
+var enemyCardDeck = []; //Колода карт противника, для взятия карт в руки
+var playerHandDeck = []; //Колода карт игрока в руках.
+var playerDeckHistory = []; //колода для истории( которая будет отображаться на "столе").
+var enemyHandDeck = []; //колода карт противника в руках.
+var enemyDeckHistory = []; //колода для истории противника.
+var shareDeck = []; //колода битвы
+var shareDeckHistory = []; //колода для истории ходов.
+
+function getRandomFromArr(arr) { // Рандомно вытаскиваем с массива index
+  return Math.floor(Math.random() * arr.length);
+};
+
+function createCard(){ //Создаем карту из преддставленного выбора.
  var randomCardNumber = card_numbers[getRandomFromArr(card_numbers)];
  var randomCardType = card_types[getRandomFromArr(card_types)];
  return [randomCardNumber, randomCardType].join('@');
-}; 
-
-function fillNewDeck(cards){                      // функция заполнения первоначальной колоды игрока\бота.
- var deck = [];
- for (var i = 0; i < cards; i++){
-  var a = createCard();
-    if (a == deck.indexOf(a)){
-   i--;
-   }
-   deck.push(a);
- }
-return deck;
 };
 
-function isNumeric(n) {                       // проверка на число :)
+//..function fillNewDeck(cards){ //старая функция заполнения начальнйо колоды на руки игрока.
+// var deck = [];
+// for (var i = 0; i < cards; i++){
+//  var a = createCard();
+//  
+//  if (a == deck.indexOf(a)){
+//   i--;
+//   }
+//   deck.push(a);
+//    
+// }
+// return deck;
+//};
+
+function isNumeric(n) {    //Проверка на число.
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
-function calculateDamage(arr){              // массив с суммами пар карт в деке боя. Дорабатываю.
+function calculateDamage(arr){ //Парный подсчет очков от карт. пока без мастей.
  var sumArr = [];
  for (var i = 0; i < arr.length; i++){
   var a = card_costs[arr[i].split("@")[0]];
@@ -58,15 +71,30 @@ function calculateDamage(arr){              // массив с суммами п
  return sumArr;
 };
 
+function createCardDeck(func, arr, cardsDeck){ // Создание колоды карт, дял взятия оттуда карт.
+ for (var i = 0; i < cardsDeck; i++){
+  var a = func;
+  if (arr.indexOf(a) < 0){
+   i--;
+  }else{
+   arr.push(a);
+  }
+ }
+ return arr;
+};
 
-function calculateDamageDiamonds(arr){    // дорабатываю.
- var sum = 0;
- var result = [];
- if (arr.length <= 1){
-  for (var i = 0; i < arr.length; i++){
-   var a = card_costs[arr[i].split("@")[0]];
-   sum += a;
-   }
-  
-  return result;
+function takeCardFromDeck(func, arr, arr2, cards){ //Так выглядит взятие карт(ы) в руки из ранее созданной колоды.
+ for (var i = 0; i < cards; i++){
+  var a = func;
+  arr.push(arr[a]);
+  arr2.splice(1, a);
+ }
+};
+
+function putCardIntoBattle(arr,card){ // Перемещение карты в колоду боя.
+ 
+};
+
+function doCardIntoHistory(){ //Перемещение отработанной карты в историю.
+ 
 };
