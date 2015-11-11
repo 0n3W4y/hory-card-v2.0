5262edbf28060f19c00d6537cc1aa6907b594712
0n3W4y <vulturetm@gmail.com>
2015-11-11 16:12:55 +0300

    Update battle.js

--- battle.js
+++ battle.js
@@ -6,18 +6,74 @@ var cardsDeck ={
  costs : {2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 11: 2, 12: 20, 13: 4, 14: 11}
 };
 
-var battleGround = { // пока глобальный объект стол, как класс не могу сделать. Может позже
- battleDeck : [], // что бы была
- battleDeckHistory : [], // лог боев
- damage : [], // приходящий массив из баттледеки игрока
- calculatedmg: function(){ // функция подсчета дамага из массива.
-  var sum = 0;
-  for (var i = 0; i < battleGround.damage.length; i++){
-   sum += battleGround.damage[i];
+var Battleground = $.klass({ // класс для полебоя
+ init: function (name){
+  this.name = name;
+  this.deck = null;
+  this.historyDeck = null;
+  
+ },
+ 
+ calculateDamage: function (player){ // функция подсчета дамага, пока без мастей и комбо
+   var sumArr = [];
+   var sum = 0;
+     for (var i = 0; i < this.deck.length; i++){
+	  if (this.deck[i+1]){ //проверка на существования последнего индекса в массиве, после каждой операции сложения.
+      var a = cardsDeck.costs[this.deck[i][0]];
+      var b = cardsDeck.costs[this.deck[i+1][0]];
+	  sumArr.push(a+b);
+	  i++      
+      }else{
+	  var a = cardsDeck.costs[this.deck[i][0]]
+      sumArr.push(a);
+      }
+     }
+	 
+   for (var i = 0; i < sumArr.length; i++){ // складываем весь дамаг, находящийся в массиве попарно.
+    sum += sumArr[i]; 
   }
-  return sum;
- }
-};
+  player.stats.HP = player.stats.HP - sum; // сразу же наносим урон игроку.
+ },
+ 
+ battleStart: function (player1, player2){ // начало Игры.
+  player1.battleDeck = [];
+  player1.deck = [];
+  player2.battleDeck = [];
+  player2.deck = [];
+  this.deck = [];
+  this.historyDeck = [];
+ },
+ 
+ battleEnd: function(){ // конец игры ( вывод сообщений подсчет очков, статистика )
+ 
+  },
+ 
+ turnStart: function (player, cards){ //начало хода для игрока
+  player.battleDeck.length = 0; // обнуляем боевую деку игрока, от предыдущих значений.
+  this.calculateDamage(player); // собираем урон, оставленный предыдущим хода другого игрока.
+  this.deck.length = 0; // после сбора урона обнуляем деку полебоя куда копируются карты из баттлдеки игрока.
+  // тут должно быть условие на предмет - умер ли персонаж после дамага или нет. 
+  this.giveCard(player, cards); // раздаем карты игроку.
+ },
+ 
+ turnEnd: function (player){ // заканчиваем ходигрока
+  this.deck = player.battleDeck; // коопируем набросанные карты из его деки в деку полебоя
+  var tempArr = [player.name, [this.deck]]; // создаем массив из хода игрока.
+  this.historyDeck = $.merge( this.historyDeck, tempArr ); // совмещаем массивы в итосрии полебоя
+  
+  },
+ 
+ giveCard: function (player, cards){ //оаздача карт игроку.
+  for (var i = 0; i < cards; i++){
+   var a = Math.floor(Math.random() * cardsDeck.numbers.length);
+   var b = Math.floor(Math.random() * cardsDeck.types.length);
+   var num = [cardsDeck.numbers[a], cardsDeck.types[b]];
+   player.deck.push(num);
+   }
+  },
+  
+  
+});
 
 function getRandomCard(){ // генератор карт
  var a = getRandomFromArr(cardsDeck.numbers);
@@ -66,59 +122,23 @@ var Stats = $.trait({ // трейт стат, для заполнения нуж
 	}
 });
 
-var Player = $.klass({ // класс для игрока
+var Player = $.klass({
   init: function(name, race) {  
   this.name = name;
   this.race = race;
   this.experience = 1;
   this.level = 1;
-  this.deck = []; // руки с картами
-  this.deckHistory = []; // лог выкладки карт игрока
-  this.battleDeck = null; // дека для боя
-  },
-  getcard: function(cards){ // взять cards карт и положить себе в  руки
-   for (var i = 0; i < cards; i++){
-	var a = Math.floor(Math.random() * cardsDeck.numbers.length);
-	var b = Math.floor(Math.random() * cardsDeck.types.length);
-	var num = [cardsDeck.numbers[a], cardsDeck.types[b]];
-	this.deck.push(num);
-   }
-  },
-  givecardtobattle: function(selectedCard){ // положить карту из рук в деку с боем
-   if (this.battleDeck === null){
-    this.battleDeck = []
-   };
-   this.battleDeck.push(selectedCard);
-   var a = this.deck.indexOf(selectedCard);
-   this.deck.splice(1, a); // удалить карту из рук
-  },
-  calculateDamage: function(){ // подсчет дамага без мастей, тупо достоинство карт.
-   var sumArr = [];
-     for (var i = 0; i < this.battleDeck.length; i++){
-      var a = cardsDeck.costs[this.battleDeck[i][0]];
-      var b = cardsDeck.costs[this.battleDeck[i+1][0]];
-      if ( isNumeric(b) ){ // проверка , четное или не четное количество карт в деке боя.
-      sumArr.push(a+b);
-      i++;
-      }else{
-      sumArr.push(a);
-      }
-     }
-   return sumArr;
-  },
-  startbattle: function(cards){ // стартбоя
-   this.battleDeck = battleGround.battleDeck; // маунтим из глобального объекта деку с боем игроку ( впринципе не нужно )
-   this.stats.HP = this.stats.HP - battleGround.calculatedmg(); // вычитаем урон их ХП, от игрока, ходившего раньше.
-   this.getcard(cards); // берем cards карт
-   
-   //alert( "Ваш ход!" ); 
-  },
-  endbattle: function(){ // окончание хода игрока
-   battleGround.battleDeckHistory = $.merge( battleGround.battleDeckHistory, this.battleDeck );//собираем массив в историю.
-   battleGround.damage = this.calculateDamage(); // считаем урон из положенных карт в декубоя, присваимаем массив столу.
-   
-   this.battleDeck.length = 0; // обнуляем деку боя ( свою )
+  this.deck = null;
+  this.deckHistory = null; // пока не задействована.( возможно не нужна будет )
+  this.battleDeck = null;
   },
   
- _traits: [Inventory, Stats], // добаляем трейты ( не работает )
+  givecardtobattle: function(selectedCard){ // передача выбранной карты в бой
+   this.battleDeck.push(selectedCard);
+   var a = this.deck.indexOf(selectedCard);
+   this.deck.splice(a, 1); //обязательное удаление карты из руки игрока.
+  },
+  
+ _traits: [Inventory, Stats],
+ 
 });
